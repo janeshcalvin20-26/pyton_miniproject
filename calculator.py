c9d6c58dbe7fb382603151d75058b01fc799d84a
janesh calvin <janeshcalvin20@gmail.com>
2026-03-23 10:08:29 +0530

    initial commit

---
+++ calculator.py
@@ -0,0 +1,10 @@
+def add(a, b):
+    return a + b
+def subtract(a, b):
+    return a - b
+def multiply(a, b):
+    return a * b
+
+print("addition:",add(10,5))
+print("subtraction:",subtract(10,5))
+print("multiplication:",multiply(10,5))
